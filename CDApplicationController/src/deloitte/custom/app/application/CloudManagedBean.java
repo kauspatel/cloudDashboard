7d01a5a29a33889f6186efc465af5bc6fd241552
kauspatel <kauspatel@192.168.56.1>
2017-06-20 11:55:38 +0530

    Added Inapp browser and Voice Recognition

--- CDApplicationController/src/deloitte/custom/app/application/CloudManagedBean.java
+++ CDApplicationController/src/deloitte/custom/app/application/CloudManagedBean.java
@@ -1,27 +1,31 @@
 package deloitte.custom.app.application;
 
 
-import java.math.BigDecimal;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
+import java.util.Map;
+
+import java.util.Set;
+
 import oracle.adfmf.amx.event.ActionEvent;
-import oracle.adfmf.amx.event.ChartDrillEvent;
-import oracle.adfmf.amx.event.SelectionEvent;
-import oracle.adfmf.bindings.dbf.AmxIteratorBinding;
 import oracle.adfmf.framework.api.AdfmfContainerUtilities;
 import oracle.adfmf.framework.api.AdfmfJavaUtilities;
-import oracle.adfmf.framework.exception.AdfInvocationException;
-import oracle.adfmf.java.beans.PropertyChangeListener;
 import oracle.adfmf.java.beans.PropertyChangeSupport;
-import oracle.adfmf.java.beans.ProviderChangeSupport;
-import oracle.adfmf.json.JSONArray;
-import oracle.adfmf.json.JSONObject;
-import oracle.adfmf.util.GenericType;
 
-import oracle.maf.api.dc.ws.rest.RestServiceAdapter;
-import oracle.maf.api.dc.ws.rest.RestServiceAdapterFactory;
+import oracle.adfmf.util.Utility;
+import java.util.Map.Entry;
 
 
 public class CloudManagedBean {
@@ -63,4 +67,81 @@ public class CloudManagedBean {
     public String getCurrentFeature() {
         return currentFeature;
     }
+
+    public String naviagateToReportUrl() {
+        // Add event code here...
+        String featureID = AdfmfJavaUtilities.getFeatureId();
+        AdfmfContainerUtilities.invokeContainerJavaScriptFunction(featureID, "navigateToReportUrl", new Object[] {AdfmfJavaUtilities.getELValue("#{pageFlowScope.currReportUrl}")});
+        return null;
+    }
+    
+    public void processSpeech(String speechText){
+        System.out.println("::::"+speechText+":::::");
+        String featureID = AdfmfJavaUtilities.getFeatureId();
+        if(speechText.length()>4){
+            String s=getFilteredReportUrl(speechText);
+            
+            if(s.length()>10){                
+                AdfmfContainerUtilities.invokeContainerJavaScriptFunction(featureID, "navigateToReportUrl", new Object[] {s});
+            }else{
+                AdfmfContainerUtilities.invokeContainerJavaScriptFunction(featureID, "showAlert", new Object[] {speechText});
+            }
+        }else{
+            AdfmfContainerUtilities.invokeContainerJavaScriptFunction(featureID, "showAlert", new Object[] {speechText});
+        }
+        
+        
+    }
+
+    public String startVoiceRecognition() {
+        // Add event code here...
+        String featureID = AdfmfJavaUtilities.getFeatureId();
+        AdfmfContainerUtilities.invokeContainerJavaScriptFunction(featureID, "StartSpeechToText", new Object[] {});
+        return null;
+    }
+    
+    public String getFilteredReportUrl(String searchKeyword) {
+        Connection conn = null;
+        String returnValue = "";
+        try {
+            String[] searchKeyworkArray=searchKeyword.split(" "); 
+            Collection listTwo = new ArrayList(Arrays.asList(searchKeyworkArray));
+            conn = ConnectionFactory.getConnection();
+            Statement stmt = conn.createStatement();
+            StringBuffer query = new StringBuffer();
+            query.append("SELECT REPORT_URL FROM REPORT_DETAILS");
+            Map<String, Integer> map = new HashMap<String, Integer>(); 
+            ResultSet result = stmt.executeQuery(query.toString());
+            while (result.next()) {
+                System.out.println("inside first while");
+                String reportUrl = (String) result.getObject("REPORT_URL");
+                String[] reportUrlArray=reportUrl.split(",");
+                Collection listOne = new ArrayList(Arrays.asList(reportUrlArray));
+                listOne.retainAll( listTwo );
+                map.put(reportUrl, listOne.size());
+            }
+            Set<Entry<String, Integer>> set = map.entrySet();
+                    List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
+                    Collections.sort( list, new Comparator<Map.Entry<String, Integer>>()
+                    {
+                        public int compare( Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2 )
+                        {
+                            return (o2.getValue()).compareTo( o1.getValue() );
+                        }
+                    } );
+            for(Map.Entry<String, Integer> entry:list){
+                        System.out.println(entry.getKey()+" ==== "+entry.getValue());
+                        returnValue=entry.getKey();
+                break;
+             }
+            
+        } catch (Exception ex) {
+            Utility.ApplicationLogger.severe(ex.getMessage());
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return returnValue;
+    }
+    
+   
 }
---
+++ CDApplicationController/src/deloitte/custom/app/application/ConnectionFactory.java
@@ -0,0 +1,38 @@
+package deloitte.custom.app.application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import oracle.adfmf.framework.api.AdfmfJavaUtilities;
+
+public class ConnectionFactory {
+    public ConnectionFactory() {
+        super();
+    }
+
+    protected static Connection conn = null;
+
+    public static Connection getConnection() throws Exception {
+        if (conn == null) {
+            try {
+                String Dir = AdfmfJavaUtilities.getDirectoryPathRoot(AdfmfJavaUtilities.ApplicationDirectory);
+                String connStr = "jdbc:sqlite:" + Dir + "/CloudDashboardApp.db";
+                conn = new SQLite.JDBCDataSource(connStr).getConnection();
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (conn != null) {
+                conn.close();
+                conn = null;
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
